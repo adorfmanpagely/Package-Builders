bfd9e7a8fa81ea9eca8c45976c78e24c2dfd3675
A. Dorfman <anthony.dorfman@pagely.com>
2016-05-13 22:48:20 +0000

    added args, multi distro support

--- php-pecl-libsodumn/build.sh
+++ php-pecl-libsodumn/build.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
+
+DISTRO='ubuntu'
+RELEASE='xenial'
+VERSION='1.0.6'
+
+for i in "$@"
+do
+    case $i in
+        -d=*|--distro=*)
+        DISTRO="${i#*=}"
+        shift 
+        ;;
+        -r=*|--release=*)
+        RELEASE="${i#*=}"
+        shift  
+        ;;
+        -v=*|--version=*)
+        VERSION="${i#*=}"
+        shift
+        ;;
+        *)
+        ;;
+    esac
+done
+
 rm -rf out
 mkdir out
-                                                                                              
-docker run --rm -v $(pwd)/build-scripts:/build-scripts:ro \
+
+docker run --rm -e DISTRO=$DISTRO \
+                -e RELEASE=$RELEASE \
+                -e VERSION=$VERSION \
+                -v $(pwd)/build-scripts:/build-scripts:ro \
                 -v $(pwd)/out:/out  \
-                -w /build-scripts ubuntu-xenial-fpm-build:1 \
+                -w /build-scripts ${DISTRO}-${RELEASE}-fpm-build:1 \
                 /build-scripts/build-package
 
 cp out/*.deb ./
